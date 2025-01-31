27eec2c2eb71adae92e7998db7a94b1fcf3d656a
Miclasher <14122006mg@gmail.com>
2025-01-31 15:49:50 +0200

    Implement caching to reduce amount of API calls

--- CurrencyBot/Services/ExchangeRateService.cs
+++ CurrencyBot/Services/ExchangeRateService.cs
@@ -1,18 +1,37 @@
 ﻿using CurrencyBot.Models;
 using System.Text.Json;
+using System.Runtime.Caching;
 
 namespace CurrencyBot.Services
 {
     public class ExchangeRateService : IExchangeRateService
     {
+        private readonly static CacheItemPolicy CacheItemPolicy = new CacheItemPolicy();
+        private readonly MemoryCache _cache = MemoryCache.Default;
         private readonly HttpClient _httpClient = new HttpClient();
+
         public async Task<ExchangeRateResponse> GetExchangeRateResponse(DateOnly date)
         {
-            var response = await FetchExchangeRateResponse(ConvertDateToUrl(date));
+            ExchangeRateResponse? response;
+            bool wasCached = false;
 
-            if (response is null || response.ExchangeRates.Count == 0)
+            if (_cache.Contains(date.ToString()))
             {
-                throw new InvalidOperationException("No exchange rates found for specified date.");
+                wasCached = true;
+                response = JsonSerializer.Deserialize<ExchangeRateResponse>((string)_cache.Get(date.ToString()));
+            }
+            else
+            {
+                response = await FetchExchangeRateResponse(ConvertDateToUrl(date));
+                if (response is null || response.ExchangeRates.Count == 0)
+                {
+                    throw new InvalidOperationException("No exchange rates found for specified date.");
+                }
+            }
+
+            if (!wasCached)
+            {
+                _cache.Set(date.ToString(), JsonSerializer.Serialize(response), CacheItemPolicy);
             }
 
             return response!;
